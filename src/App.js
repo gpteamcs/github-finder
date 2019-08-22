0cb290134a630750f734bd326206f5f61cf5bfe6
Ahmed Maamoun <Ahmed.Maamoun@itworx.com>
2019-08-22 16:32:02 +0200

    adding some conditional logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "Ahmed Maamoun";
+    const loading = false;
+    const showName = true;
+    if (loading) {
+      return <h4>loading...</h4>;
+    }
     return (
-      <div className="App">
-        <h1>Hello From React</h1>
-      </div>
+      <Fragment>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+      </Fragment>
     );
   }
 }
