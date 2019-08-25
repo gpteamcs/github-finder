aed64b07a801ec6ce26816f5272f06842ef13cbd
ahmed maamoun <ahmedfcisasu@gmai.com>
2019-08-25 18:34:24 +0200

    Add bindings

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Component, Fragment } from "react";
-import "./App.css";
+import React, { Component, Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './users/UserItem';
+import './App.css';
 
 class App extends Component {
   render() {
-    const name = "Ahmed Maamoun";
-    const loading = false;
-    const showName = true;
-    if (loading) {
-      return <h4>loading....</h4>;
-    }
     return (
       <Fragment>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <UserItem />
       </Fragment>
     );
   }
