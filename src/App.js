4cdfed8ea8fe85b00df479e27d880fc572207f8e
Ahmed Maamoun <Ahmed.Maamoun@itworx.com>
2019-08-26 16:11:44 +0200

    Section 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { Component, Fragment } from 'react';
-import Navbar from './components/layout/Navbar';
-import UserItem from './users/UserItem';
-import './App.css';
-
+import React, { Component, Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./users/Users";
+import "./App.css";
+import axios from "axios";
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ loading: false, users: res.data });
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar title="Github Finder" icon="fab fa-github" />
-        <UserItem />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </Fragment>
     );
   }
